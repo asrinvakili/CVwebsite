425cd00d5b60ddf47d45431ba6e60a3c1fac0e45
asrin_vakili <asrin_vakili@yahoo.com>
2024-01-30 14:56:01 +0300

    setting up blog address

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import blog
+from blog.views import blog, single_blog
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', blog, name='weblog'),
+    path('singl', single_blog, name='singleblog')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog.html')
+    return render(request, 'blog/blog.html')
+
+
+def single_blog(request):
+    return render(request, 'blog/single-blog.html')
