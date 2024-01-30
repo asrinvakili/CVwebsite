9c86cb3d3f344412f1f4495df1aa5cb2684e4af6
asrin_vakili <asrin_vakili@yahoo.com>
2024-01-30 20:12:49 +0300

    setting up contact and blog info

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
+contex = {
+    'name': 'Asrin Vakili',
+    'time': '1/30/2024',
+    'view': '2.3M Views',
+    'comment': '13 Comments'
+}
 def blog(request):
-    return render(request, 'blog/blog.html')
+    return render(request, 'blog/blog.html', contex)
 
 
 def single_blog(request):
-    return render(request, 'blog/single-blog.html')
+    return render(request, 'blog/single-blog.html', contex)
