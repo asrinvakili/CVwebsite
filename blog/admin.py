6a318535c428f6dcf1733e1c2b4bdcd7c396f193
asrin_vakili <asrin_vakili@yahoo.com>
2024-02-03 00:37:17 +0300

    created post table
    install pillow
    created post table
    setting up blog admin setting

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
 # Register your models here.
+class blogAdmin(admin.ModelAdmin):
+    date_hierarchy = 'public_date'
+    empty_value_display = '-empty-'
+    fields = ('title', 'slug', 'public_date', 'content', 'author', 'tags')
+    list_display = ('title', 'slug', 'public_date', 'author', 'tags')
+    list_filter = ('public_date', 'author', 'status')
+    search_fields = ('title', 'author', 'status', 'public_date')
