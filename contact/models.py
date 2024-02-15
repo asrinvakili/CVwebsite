6966f3e1d009fb0ce14d6ce0917c6197e05fc872
asrin_vakili <asrin_vakili@yahoo.com>
2024-02-16 02:28:03 +0300

    null= True

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=100, default="Unknown")
-    subject = models.CharField(max_length=100, blank=True)
+    subject = models.CharField(max_length=100,null=True, blank=True)
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
