554c33acf25e4f14e74b1ea65302e98cfd921ce7
asrin_vakili <asrin_vakili@yahoo.com>
2024-02-21 20:18:23 +0300

    setting up huminize

--- CVwebsite/settings.py
+++ CVwebsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
     'home.apps.HomeConfig',
     'about.apps.AboutConfig',
     'blog.apps.BlogConfig',
